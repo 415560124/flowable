d88830064b00ab2e11edfbabf1f88149fdd2cfea
rhy <415560124@qq.com>
2023-10-25 22:51:06 +0800

    1.修改项目配置及SQL

--- src/main/java/com/rhy/flowable/FlowableApplication.java
+++ src/main/java/com/rhy/flowable/FlowableApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlowableApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlowableApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableApplication.class, args);
+    }
 
 }
